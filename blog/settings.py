c2e75869924d2730984bcdd4dc865cb4267e3b4b
tristanlimyj <tristanlimyj@gmail.com>
2020-12-17 13:00:27 +0800

    debug on

--- blog/settings.py
+++ blog/settings.py
@@ -15,7 +15,7 @@ if os.getenv('SETTING_TYPE') == 'Development':
     CSRF_COOKIE_SECURE = False
 else:
     ALLOWED_HOSTS = [".goodfunfriday.com", "139.59.112.156"]
-    DEBUG = False
+    DEBUG = True
     SECURE_SSL_REDIRECT = True
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
