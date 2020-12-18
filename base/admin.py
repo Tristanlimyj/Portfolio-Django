852621d073f32e56ea63b1e8190613399d7c9050
tristanlimyj <tristanlimyj@gmail.com>
2020-12-18 16:44:38 +0800

    Revert "changes something weird"
    This reverts commit e69e00fe6cbd3edc0a4f11ed39082a92fee7ed19.

--- base/admin.py
+++ base/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, User
+from .views import TwoFAAdmin
+from django_otp.plugins.otp_totp.models import TOTPDevice
 
 # No 2FA Edits
 admin.site.site_header = "Dashboard"
 admin.site.unregister(Group)
 
-
+# Registering for the Production
+admin_site = TwoFAAdmin(name="OTPAdmin")
+admin_site = admin_site.site_header = "Dashboard"
+admin_site.register(User)
+admin_site.register(TOTPDevice)
 
--- project/admin.py
+++ project/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from ../base/admin import admin_site
 from .models import Project
 
 class ProjectAdmin(admin.ModelAdmin):
@@ -6,3 +7,4 @@ class ProjectAdmin(admin.ModelAdmin):
     list_filter = ("time_stamp",)
 
 admin.site.register(Project, ProjectAdmin)
+admin_site.register(Project, ProjectAdmin)
